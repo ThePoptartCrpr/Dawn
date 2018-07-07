f53e03a686451eda9fcf9b4a5463204bcd95f13e
ThePoptartCrpr <ThePoptartCrpr@users.noreply.github.com>
2018-07-07 16:04:17 -0700

    Base mod created

---
+++ src/main/java/com/thepoptartcrpr/dawn/Dawn.java
@@ -0,0 +1,14 @@
+package com.thepoptartcrpr.dawn;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(modid = Dawn.Reference.MODID, name = Dawn.Reference.NAME, version = Dawn.Reference.VERSION)
+public class Dawn {
+
+    public static class Reference {
+        public static final String NAME = "Dawn";
+        public static final String MODID = "dawn";
+        public static final String VERSION = "0.0.1";
+    }
+
+}
