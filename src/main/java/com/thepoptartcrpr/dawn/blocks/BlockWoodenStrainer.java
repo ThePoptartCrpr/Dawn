98019edb0e1f31b9b5df7082b356380d7b74b10a
ThePoptartCrpr <ThePoptartCrpr@users.noreply.github.com>
2018-07-10 14:02:33 -0700

    Adjust bounding box of wooden strainer

--- src/main/java/com/thepoptartcrpr/dawn/blocks/BlockWoodenStrainer.java
+++ src/main/java/com/thepoptartcrpr/dawn/blocks/BlockWoodenStrainer.java
@@ -5,14 +5,21 @@ import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.math.AxisAlignedBB;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
 
 public class BlockWoodenStrainer extends Block {
+
+    public AxisAlignedBB bounds;
 
     public BlockWoodenStrainer() {
         super(Material.WOOD);
 
         this.setUnlocalizedName("wooden_strainer");
         this.setRegistryName(new ResourceLocation(Dawn.Reference.MODID, "wooden_strainer"));
+
+        this.bounds = new AxisAlignedBB(1.5D/16.0D, 0.0D, 1.5D/16.0D, 14.5D/16.0D, 4.0D/16.0D, 14.5D/16.0D);
     }
 
     @Override
@@ -23,6 +30,16 @@ public class BlockWoodenStrainer extends Block {
     @Override
     public boolean isOpaqueCube(IBlockState state) {
         return false;
+    }
+
+    @Override
+    public AxisAlignedBB getBoundingBox(IBlockState state, IBlockAccess world, BlockPos pos) {
+        return this.bounds;
+    }
+
+    @Override
+    public AxisAlignedBB getCollisionBoundingBox(IBlockState state, IBlockAccess world, BlockPos pos) {
+        return this.bounds;
     }
 
 }
