806c2924500764f109b5e71a8a500cf45e2797ed
ThePoptartCrpr <ThePoptartCrpr@users.noreply.github.com>
2018-07-10 13:19:44 -0700

    Updated Wooden Strainer block model

--- src/main/java/com/thepoptartcrpr/dawn/blocks/BlockWoodenStrainer.java
+++ src/main/java/com/thepoptartcrpr/dawn/blocks/BlockWoodenStrainer.java
@@ -4,7 +4,6 @@ import com.thepoptartcrpr.dawn.Dawn;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
-import net.minecraft.util.BlockRenderLayer;
 import net.minecraft.util.ResourceLocation;
 
 public class BlockWoodenStrainer extends Block {
