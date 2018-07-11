021bc41826f0a994cd1dce054c1c66467c1706e2
ThePoptartCrpr <ThePoptartCrpr@users.noreply.github.com>
2018-07-10 17:08:00 -0700

    Strainers can only be placed on a solid block now

--- src/main/java/com/thepoptartcrpr/dawn/blocks/BlockWoodenStrainer.java
+++ src/main/java/com/thepoptartcrpr/dawn/blocks/BlockWoodenStrainer.java
@@ -8,18 +8,26 @@ import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
+import net.minecraft.world.World;
 
 public class BlockWoodenStrainer extends Block {
 
     public AxisAlignedBB bounds;
 
     public BlockWoodenStrainer() {
-        super(Material.WOOD);
+        // Sneaky workaround so you can't place two on top of the other
+        super(Material.CIRCUITS);
 
         this.setUnlocalizedName("wooden_strainer");
         this.setRegistryName(new ResourceLocation(Dawn.Reference.MODID, "wooden_strainer"));
 
         this.bounds = new AxisAlignedBB(1.5D/16.0D, 0.0D, 1.5D/16.0D, 14.5D/16.0D, 4.0D/16.0D, 14.5D/16.0D);
+    }
+
+    @Override
+    public boolean canPlaceBlockAt(World world, BlockPos pos)
+    {
+        return (world.getBlockState(pos.down()).getMaterial().isSolid() && super.canPlaceBlockAt(world, pos));
     }
 
     @Override
